aa1b93ef3603b288681cb095415da651f7a3b5ba
Robert D Almeida <robert.dalmeida@ing.com>
2021-02-20 17:28:09 +0100

    Just adding one more parameter.

--- jsonparsing.playground/Contents.swift
+++ jsonparsing.playground/Contents.swift
@@ -5,11 +5,11 @@ var json = Data("""
 {
     "results": [
     {"type":"Plant", "name":"Strawberry"},
-    {"type":"Human", "name":"Robert"},
-    {"type":"Human", "name":"Clarence"},
-    {"type":"Animal", "name":"Dog"},
-    {"type":"Animal", "name":"Cat"},
-    {"type":"Animal", "name":"Rat"}
+    {"type":"Human", "name":"Robert", "age": 11},
+    {"type":"Human", "name":"Clarence", "age": 20},
+    {"type":"Animal", "name":"Dog", "age": 3},
+    {"type":"Animal", "name":"Cat", "age": 4},
+    {"type":"Animal", "name":"Rat", "age": 5}
     ]
 }
 """.utf8)
@@ -21,7 +21,7 @@ var response = try? JSONDecoder().decode(Response.self, from: json)
 protocol DataSource {
     associatedtype Item
     func fetchPlants() -> [Item]
-    func fetchHumans() -> [Item]
+    func fetchAdultAgedHumans() -> [Item] //Anyone who is older than 18 years.
     func fetchAnimals() -> [Item]
 }
 
@@ -33,7 +33,7 @@ struct Provider: DataSource {
     func fetchPlants() -> [Identity] {
         return []
     }
-    func fetchHumans() -> [Identity] {
+    func fetchAdultAgedHumans() -> [Identity] {
         return []
     }
     func fetchAnimals() -> [Identity] {
@@ -52,7 +52,7 @@ var source: some DataSource = createDataSource()
 if source.fetchPlants().count != 1 {
     print("Fail")
 }
-if source.fetchHumans().count != 2 {
+if source.fetchAdultAgedHumans().count != 1 {
     print("Fail")
 }
 if source.fetchAnimals().count != 3 {
